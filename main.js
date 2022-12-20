709d88c29b9b72e2bc6f35d2e2e6c636ca62a028
Kris <kharonek@yahoo.com>
2022-12-20 14:40:46 +0000

    offset added

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 let width = 0;
 let height = 0;
-let scale = 27
-let octaves = 4
-let presistance = 0.5
-let lacunarity = 1.87
+let scale = 27;
+let octaves = 4;
+let presistance = 0.5;
+let lacunarity = 1.87;
+let seed = 100;
+let offset = {x:0, y:0};
 
-let noiseMap = new NoiseScript().generateNoiseMap(canvas.width, canvas.height, scale, octaves, presistance, lacunarity);
+let noiseMap = new NoiseScript().generateNoiseMap(canvas.width, canvas.height, scale, octaves, presistance, lacunarity, seed, offset);
 let colorMap = new MapGenerator(canvas.width, canvas.height, ctx, noiseMap);
 colorMap.drawNoise();
 
@@ -23,15 +25,19 @@ const scaleSlider = document.getElementById("scaleSlider");
 const octavesSlider = document.getElementById("octavesSlider");
 const presistanceSlider = document.getElementById("presistanceSlider");
 const lacunaritySlider = document.getElementById("lacunaritySlider");
+const seedSlider = document.getElementById("seedSlider");
+const offsetXSlider = document.getElementById("offsetXSlider");
+const offsetYSlider = document.getElementById("offsetYSlider");
 
 button.addEventListener('click', function handleClick() {
   scale = scaleSlider.value;
   octaves = octavesSlider.value;
   presistance = presistanceSlider.value;
   lacunarity = lacunaritySlider.value;
-  console.log(lacunarity);
+  seed = seedSlider.value;
+  offset = {x:parseInt(offsetXSlider.value), y:parseInt(offsetYSlider.value)};
 
-  let noiseMap = new NoiseScript().generateNoiseMap(canvas.width, canvas.height, scale, octaves, presistance, lacunarity);
+  let noiseMap = new NoiseScript().generateNoiseMap(canvas.width, canvas.height, scale, octaves, presistance, lacunarity, seed, offset);
   let colorMap = new MapGenerator(canvas.width, canvas.height, ctx, noiseMap);
   colorMap.drawNoise();
 });
